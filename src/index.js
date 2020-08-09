0a1dcc0f1991f21e4810c20157b48cf3c2e35015
Arthur Passos <arthur.pcorrea@sempreceub.com>
2020-08-09 16:31:32 -0300

    Adicionando ESLint

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ const express = require('express');
 const routes = require('./routes');
 
 const passport = require('passport');
-const session = require("express-session");
-const bodyParser = require("body-parser");
+const session = require('express-session');
+const bodyParser = require('body-parser');
 const userController = require('./controllers/UserController');
 const bcrypt = require('bcrypt');
 const LocalStrategy = require('passport-local').Strategy;
@@ -14,40 +14,36 @@ app.use(express.json());
 app.use(routes);
 
 passport.use(new LocalStrategy({
-    usernameField: 'email'
+  usernameField: 'email',
 },
-    function(email, password, done) {
-        console.log(email, password);
-        userController.findByEmail(email).then(async (err, user) => {
-            if (err) {
-                return done(err, {message: "Erro"});
-            }
-            if (!user) {
-                console.log("Usuario nao encontrado")
-                return done(null, false, { message: 'Usuário não existe' });
-            }
-            console.log("Usuario encontrado")
-            try {
-                if(await bcrypt.compare(password, user.hashPassword)){
-                    console.log("Login realizado")
-                    return done(null, user)
-                } else {
-                    return done(null, false, { message: 'Senha incorreta' })
-                } 
-            } catch(e) {
-                return done(e);
-            }
-        });        
-    },
-));
+((email, password, done) => {
+  console.log(email, password);
+  userController.findByEmail(email).then(async (err, user) => {
+    if (err) {
+      return done(err, { message: 'Erro' });
+    }
+    if (!user) {
+      console.log('Usuario nao encontrado');
+      return done(null, false, { message: 'Usuário não existe' });
+    }
+    console.log('Usuario encontrado');
+    try {
+      if (await bcrypt.compare(password, user.hashPassword)) {
+        console.log('Login realizado');
+        return done(null, user);
+      }
+      return done(null, false, { message: 'Senha incorreta' });
+    } catch (e) {
+      return done(e);
+    }
+  });
+})));
 
 passport.serializeUser((user, cb) => cb(null, user.id));
-  
-passport.deserializeUser((id, cb) => userController.findById(id, (err, user) =>
-    err ? cb(err) : cb(null, user)
-));
 
-app.use(express.static("public"));
+passport.deserializeUser((id, cb) => userController.findById(id, (err, user) => (err ? cb(err) : cb(null, user))));
+
+app.use(express.static('public'));
 app.use(session({ secret: 'segredo', resave: false, saveUninitialized: false }));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(passport.initialize());
